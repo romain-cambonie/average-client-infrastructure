1e6576638368851f6cbb2419006307c4613e2967
Romain Cambonie <romain.cambonie@gmail.com>
2022-10-17 03:00:51 +0200

    feat(acl): deactivating public access block for now as its block the files sync

--- s3.tf
+++ s3.tf
@@ -12,8 +12,8 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.client.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
